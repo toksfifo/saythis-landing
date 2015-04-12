7c813f4c18cdf35ef712eb1fa18d458522328f85
Toks Fifo <toksfifo@gmail.com>
2015-04-12 16:58:58 -0400

    basic home layout

--- client/js/routes/routes.js
+++ client/js/routes/routes.js
@@ -1,4 +1,4 @@
-app.config(['$routeProvider', function($routeProvider) {
+app.config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 	$routeProvider.
 		when('/', {
 			templateUrl: 'client/templates/home.html',
@@ -7,4 +7,10 @@ app.config(['$routeProvider', function($routeProvider) {
 		otherwise({
 			redirectTo: '/'
 		});
+
+	// remove default angular # in URL
+	// $locationProvider.html5Mode({
+	// 	enabled: true
+	// });
+
 }]);
