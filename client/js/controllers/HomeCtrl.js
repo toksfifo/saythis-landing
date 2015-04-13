bc9a05958d345bf91cf94b8f75ab79734587dab4
Toks Fifo <toksfifo@gmail.com>
2015-04-13 00:56:45 -0400

    added tweenlite animation

--- client/js/controllers/HomeCtrl.js
+++ client/js/controllers/HomeCtrl.js
@@ -1,3 +1,13 @@
 app.controller('HomeCtrl', ['$scope', function($scope) {
 
+	init();
+
+	function init() {
+		var splashContent = document.getElementsByClassName("splash-content")[0];
+		TweenLite.to(splashContent, 2, {
+			opacity: 1,
+			ease: Power2.easeOut
+		});
+	}
+
 }]);
