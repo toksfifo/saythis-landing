4185e14a9b39d3391430acb8844759e4b05e4948
Toks Fifo <toksfifo@gmail.com>
2015-05-17 14:06:40 -0400

    gulp less

--- gulpfile.js
+++ gulpfile.js
@@ -5,10 +5,13 @@ var gulp = require('gulp');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
+var less = require('gulp-less');
+var minifyCss = require('gulp-minify-css');
+var autoprefixer = require('gulp-autoprefixer');
 
 // Concatenate & Minify JS
-gulp.task('scripts', function() {
-    return gulp.src(['client/js/*.js', 'client/js/*/*.js'])
+gulp.task('js', function() {
+    return gulp.src('client/js/**/*.js')
         .pipe(concat('app.js'))
         .pipe(gulp.dest('dist'))
         .pipe(rename('app.min.js'))
@@ -16,10 +19,21 @@ gulp.task('scripts', function() {
         .pipe(gulp.dest('dist'));
 });
 
+gulp.task('less', function() {
+    return gulp.src('client/css/style.less')
+        .pipe(less())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('dist'))
+        .pipe(rename('style.min.css'))
+        .pipe(minifyCss())
+        .pipe(gulp.dest('dist'));
+});
+
 // Watch Files For Changes
 gulp.task('watch', function() {
-    gulp.watch(['client/js/*.js', 'client/js/*/*.js'], ['scripts']);
+    gulp.watch('client/js/**/*.js', ['js']);
+    gulp.watch('client/css/*.less', ['less']);
 });
 
 // Default Task
-gulp.task('default', ['scripts']);
+gulp.task('default', ['js', 'less']);
